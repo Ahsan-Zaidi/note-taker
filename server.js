ff65bb290f09d1f54ab6aeb373f0fef2bfb2b9ab
Zaidi <zaidiahsan.r@gmail.com>
2022-08-07 20:51:20 -0500

    variable port

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const fs = require("fs");
 const path = require('path');
 const { v4: uuidv4 } = require('uuid');
 
-const app = express();
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 const readFile = () => {
     return JSON.parse(fs.readFileSync("./db/db.json", "utf-8", err => {
@@ -42,6 +44,6 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
 });
